74ef5eeac77d80e36871583f9e6d4102c7126908
Denis <den.halapsis@gmail.com>
2023-03-04 20:50:25 +0200

    1

---
+++ ListExample/ListExample/ListExample.cpp
@@ -0,0 +1,11 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <list>
+using namespace std;
+int main() {
+	list<int> my_list = { 12, 5, 10, 9 };
+
+	for (int x : my_list) {
+		cout << x << '\n';
+	}
+}
---
+++ StackExample/StackExample/StackExample.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream> 
+#include <stack>
+using namespace std;
+int main() {
+    stack<int> stack;
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    stack.push(4);
+    while (!stack.empty()) {
+        cout << stack.top() << " ";
+        stack.pop();
+    }
+}
